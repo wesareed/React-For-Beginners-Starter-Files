600bc0bb40338eae4b78df13c206f700ccf75c7a
Wes Reed <wes@wesreed.co>
2018-03-20 12:27:35 -0400

    update order state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -25,13 +25,29 @@ class App extends React.Component {
         this.setState({fishes: sampleFishes })
     }
 
+    addToOrder = (key) => {
+        // 1. Take a cope of existing state
+        const order = {...this.state.order};
+        // 2. Add to the order, or update the amount in order
+        order[key] = order[key] + 1 || 1;
+        // 3. Call setState to update the state object
+        this.setState({ order });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
                     <ul className="fishes">
-                     {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+                        {Object.keys(this.state.fishes).map(key => (
+                            <Fish 
+                                key={key}
+                                index={key} 
+                                details={this.state.fishes[key]} 
+                                addToOrder={this.addToOrder} 
+                            />
+                        ))}
                     </ul>
                 </div>
                 <Order />
--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -4,6 +4,8 @@ import { formatPrice } from '../helpers';
 class Fish extends React.Component {
     render() {
         const name = this.props.details.name;
+        // const status = this.props.details.status;
+        const isAvailable = this.props.details.status === "available";
         return (
             <li className="menu-fish">
                 <img src={this.props.details.image} alt={name}/>
@@ -12,7 +14,9 @@ class Fish extends React.Component {
                     <span className="price">{formatPrice(this.props.details.price)}</span>
                 </h3>
                 <p>{this.props.details.desc}</p>
-                <button>Add to Cart</button>
+                <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+                    {isAvailable ? 'Add To Order' : 'Sold Out!'}
+                </button>
             </li>
         );
     }
