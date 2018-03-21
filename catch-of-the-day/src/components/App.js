f428bf73b40a3e6cbb82635a816d44de56a27e33
Wes Reed <wes@wesreed.co>
2018-03-21 16:01:33 -0400

    persist data with local storage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -14,10 +14,22 @@ class App extends React.Component {
 
     componentDidMount() {
         const {params}  = this.props.match;
+        // reinstate local storage first
+        const localStorageRef = localStorage.getItem(params.storeId)
+        if(localStorageRef) {
+            this.setState({ order: JSON.parse(localStorageRef)}); 
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: 'fishes'
         });
+    }
+
+    componentDidUpdate() {
+        localStorage.setItem(
+            this.props.match.params.storeId,
+            JSON.stringify(this.state.order)
+        );
     }
 
     componentWillUnmount() {
